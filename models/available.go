f7039676f11d6125078358883845f000c0592afc
langhun <langhun@gmail.com>
2021-09-02 02:18:59 +0800

    x

--- models/available.go
+++ models/available.go
@@ -164,7 +164,7 @@ func CookieOK(ck *JdCookie) bool {
 			if ck.Available == True {
 				ck.Push(fmt.Sprintf("失效账号，%s", ck.PtPin))
 				JdCookie{}.Push(fmt.Sprintf("失效账号，%s", ck.Nickname))
-				var pinwskey = fmt.Sprintf("pin=%s;", ck.PtPin)
+				var pinwskey = fmt.Sprintf("pin=%s;", ck.PtPin, "wskey=%s;", ck.WsKey)
 				ck.Push(fmt.Sprintf(pinwskey))
 				msg1 := cmd(fmt.Sprintf(`wskey="%s" python3 wspt.py`, pinwskey), &Sender{})
 				fmt.Sprintf("测试1，%s", msg1)
