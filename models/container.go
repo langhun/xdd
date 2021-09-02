e44fcba7ce545d88030482bc402cb8b8dcbba44d
langhun <langhun@gmail.com>
2021-09-02 12:57:45 +0800

    x

--- models/container.go
+++ models/container.go
@@ -410,7 +410,7 @@ func (c *Container) request(ss ...string) ([]byte, error) {
 			}
 		}
 	}
-	return []byte{}, nil
+	//return []byte{}, nil
 }
 
 func GetQlVersion(address string) (string, error) {
