e14e7ef9a027fb017d1d1c8d6fa092b969a0fd05
langhun <langhun@gmail.com>
2021-09-07 01:47:40 +0800

    x

--- models/bot.go
+++ models/bot.go
@@ -161,7 +161,7 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 						PtPin: s[1],
 						WsKey: s[2],
 					}
-					if HasWsKeys(ck.WsKey) {
+					if HasPin(ck.PtPin) {
 						sender.Reply(fmt.Sprintf("11111已有wskey，开始转换..."))
 						if nck, err := GetJdCookie(ck.PtPin); err == nil {
 							sender.Reply(fmt.Sprintf("已有wskey，开始转换..."))
