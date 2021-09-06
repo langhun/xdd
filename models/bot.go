1b22843d75e076f47b309a554470c9469f3470b5
langhun <langhun@gmail.com>
2021-09-07 03:17:03 +0800

    x

--- models/bot.go
+++ models/bot.go
@@ -189,6 +189,7 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 						sender.Reply(fmt.Sprintf("没有wskey，"))
 						wstopt := simpleCmd(fmt.Sprintf(`wskey="pin=%s;wskey=%s;" python3 wspt.py`, ck.PtPin,ck.WsKey))
 						wspt := fmt.Sprintf(`"wskey=%s;%s"`, ck.WsKey, wstopt)
+						sender.Reply(fmt.Sprintf("没有wskey，\n%s",wspt))
 						ss1 := regexp.MustCompile(`wskey=([^;=\s]+);pt_key=([^;=\s]+);pt_pin=([^;=\s]+);`).FindStringSubmatch(wspt)
 						if ss1 != nil {
 							ck1 := JdCookie{WsKey: ss1[1], PtPin: ck.PtPin, PtKey: ss1[2]}
