fcd651fd9cce178e754e324fb0dff5a1fd88eff5
langhun <langhun@gmail.com>
2021-09-17 05:13:29 +0800

    x

--- models/version.go
+++ models/version.go
@@ -11,7 +11,7 @@ import (
 	"github.com/beego/beego/v2/core/logs"
 )
 
-var version = "202109705"
+var version = "2021091705"
 var describe = "江湖再见～"
 var AppName = "xdd"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
