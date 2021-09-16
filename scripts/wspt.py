4d130ad251b7d6988eca5fbfab18540616b5d5c4
langhun <langhun@gmail.com>
2021-09-17 01:07:05 +0800

    x

--- scripts/wspt.py
+++ scripts/wspt.py
@@ -3,19 +3,35 @@ import sys
 requests.packages.urllib3.disable_warnings()
 ws=sys.argv[1]
 def getsign():
-    try:
-        ## sign = 'https://hellodns.coding.net/p/sign/d/jsign/git/raw/master/sign'
+    #try:
+    #    ## sign = 'https://hellodns.coding.net/p/sign/d/jsign/git/raw/master/sign'
 
-        sign = 'https://api.jds.codes/gentoken'
-        s = requests.get(url=sign).json()
-        svv = s['sv']
-        stt = s['st']
-        suid = s['uuid']
-        sign = s['sign']
-        return svv, stt, suid, sign
+    #    sign = 'https://api.jds.codes/gentoken'
+    #    s = requests.get(url=sign).json()
+    #    svv = s['sv']
+    #    stt = s['st']
+    #    suid = s['uuid']
+     #   sign = s['sign']
+     #   return svv, stt, suid, sign
+    #except:
+    #    print('调用sign出错！')
+    try:
+    url = "https://api.jds.codes/gentoken"
+    body = {"url": "https://home.m.jd.com/myJd/newhome.action"}
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+        "Content-Type":"application/json"
+    }
+    r = requests.post(url, headers=headers, data=json.dumps(body))
+    r = json.loads(r.text)
+    data=r["data"]["sign"].split("&")
+            svv = data[5]
+            stt = data[4]
+            suid = data[1]
+            sign = data[6]
+            return svv, stt, suid, sign
     except:
         print('调用sign出错！')
-
 
 def getToken(wskey):
     sv, st, uuid, sign = getsign()
