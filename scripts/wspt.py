1bb819d1b8945cb524f041c380590470bebef145
langhun <langhun@gmail.com>
2021-09-17 01:16:04 +0800

    x

--- scripts/wspt.py
+++ scripts/wspt.py
@@ -25,8 +25,11 @@ def getsign():
             stt = data[4]
             suid = data[1]
             sign = data[6]
+                    print(svv)
+                    print(stt)
+                    print(suid)
+                    print(sign)
             return svv, stt, suid, sign
-            print(svv, stt, suid, sign)
     except:
         print('调用sign出错！')
 
