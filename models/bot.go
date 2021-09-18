3074db98bf5d1192e89339b556c9892cd0382a60
langhun <langhun@gmail.com>
2021-09-19 04:24:20 +0800

    x

--- models/bot.go
+++ models/bot.go
@@ -148,86 +148,81 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 				return nil
 			}
 		}
-		if strings.Contains(msg, "wskey=") {
-			rsp := cmd(fmt.Sprintf(`python3 wspt.py "%s"`, msg), &Sender{})
-			logs.Info(rsp)
-			ss1 := regexp.MustCompile(`pin=([^;=\s]+);wskey=([^;=\s]+)`).FindAllStringSubmatch(msg, -1)
-			if strings.Contains(rsp, "错误") {
-				logs.Error("wskey错误")
-				sender.Reply(fmt.Sprintf("wskey错误"))
-			} else {
-
-				if len(ss1) > 0 {
-					for _, s := range ss1 {
+		{
+			ss := regexp.MustCompile(`pin=([^;=\s]+);wskey=([^;=\s]+)`).FindAllStringSubmatch(msg, -1)
+			if len(ss) > 0 {
+				for _, s := range ss {
+					wkey := "pin=" + s[1] + ";wskey=" + s[2] + ";"
+					rsp := cmd(fmt.Sprintf(`python3 wspt.py "%s"`, wkey), &Sender{})
+					if strings.Contains(rsp, "错误") {
+						logs.Error("wskey错误")
+						sender.Reply(fmt.Sprintf("wskey错误"))
+					} else {
+						ptKey := FetchJdCookieValue("pt_key", rsp)
+						ptPin := FetchJdCookieValue("pt_pin", rsp)
 						ck := JdCookie{
-							PtPin: s[1],
-							PtKey: rsp,
+							PtPin: ptPin,
+							PtKey: ptKey,
 							WsKey: s[2],
 						}
+						if CookieOK(&ck) {
 
-						ss := regexp.MustCompile(`pt_key=([^;=\s]+);pt_pin=([^;=\s]+)`).FindAllStringSubmatch(rsp, -1)
-						for _, s1 := range ss {
-							ck.PtPin = s1[2]
-							ck.PtKey = s1[1]
-						}
-
-						if sender.IsQQ() {
-							ck.QQ = sender.UserID
-						} else if sender.IsTG() {
-							ck.Telegram = sender.UserID
-						}
-						if nck, err := GetJdCookie(ck.PtPin); err == nil {
-							nck.InPool(ck.PtKey)
-							if nck.WsKey == "" || len(nck.WsKey) == 0 {
-								nck.Updates(JdCookie{
-									WsKey: ck.WsKey,
-								})
-								if sender.IsQQ() {
-									ck.Update(QQ, ck.QQ)
-								}
+							if sender.IsQQ() {
+								ck.QQ = sender.UserID
+							} else if sender.IsTG() {
+								ck.Telegram = sender.UserID
+							}
+							if nck, err := GetJdCookie(ck.PtPin); err == nil {
+								nck.InPool(ck.PtKey)
 								nck.Update(PtKey, ck.PtKey)
-								msg := fmt.Sprintf("写入WsKey，并更新账号%s", ck.PtPin)
-								sender.Reply(fmt.Sprintf(msg))
-								(&JdCookie{}).Push(msg)
-								logs.Info(msg)
-							} else {
-								if nck.WsKey == ck.WsKey {
-									msg := fmt.Sprintf("重复wskey,更新ptkey")
-									nck.Updates(JdCookie{
-										PtKey: ck.PtKey,
-									})
-									sender.Reply(fmt.Sprintf(msg))
-									//(&JdCookie{}).Push(msg)
-									logs.Info(msg)
-								} else {
-									nck.Updates(JdCookie{
-										WsKey: ck.WsKey,
-									})
-									msg := fmt.Sprintf("更新WsKey，并更新账号%s", ck.PtPin)
+
+								if nck.WsKey == "" || len(nck.WsKey) == 0 {
+									if sender.IsQQ() {
+										ck.Update(QQ, ck.QQ)
+									}
+									nck.Update(WsKey, ck.WsKey)
+									msg := fmt.Sprintf("写入WsKey，并更新账号%s", ck.PtPin)
 									sender.Reply(fmt.Sprintf(msg))
 									(&JdCookie{}).Push(msg)
 									logs.Info(msg)
+								} else {
+									if nck.WsKey == ck.WsKey {
+										msg := fmt.Sprintf("重复写入")
+										sender.Reply(fmt.Sprintf(msg))
+										(&JdCookie{}).Push(msg)
+										logs.Info(msg)
+									} else {
+										nck.Updates(JdCookie{
+											WsKey: ck.WsKey,
+										})
+										msg := fmt.Sprintf("更新WsKey，并更新账号%s", ck.PtPin)
+										sender.Reply(fmt.Sprintf(msg))
+										(&JdCookie{}).Push(msg)
+										logs.Info(msg)
+									}
 								}
-							}
-						} else {
-							NewJdCookie(&ck)
-							msg := fmt.Sprintf("添加账号成功，用户名：%s\n ptkey=%s", ck.PtPin, ck.PtKey)
-							if sender.IsQQ() {
-								ck.Update(QQ, ck.QQ)
-							}
-							sender.Reply(fmt.Sprintf(msg))
-							sender.Reply(ck.Query())
-							logs.Info(msg)
-						}
 
+							} else {
+								NewJdCookie(&ck)
+
+								msg := fmt.Sprintf("添加账号，账号名:%s", ck.PtPin)
+
+								if sender.IsQQ() {
+									ck.Update(QQ, ck.QQ)
+								}
+
+								sender.Reply(fmt.Sprintf(msg))
+								sender.Reply(ck.Query())
+								(&JdCookie{}).Push(msg)
+							}
+						}
+						go func() {
+							Save <- &JdCookie{}
+						}()
+						return nil
 					}
-					go func() {
-						Save <- &JdCookie{}
-					}()
-					return nil
 				}
 			}
-
 		}
 		{
 			o := findShareCode(msg)
