e3c0e05b8c0f64d4159688fea15b5ee76cdf2f7e
langhun <langhun@gmail.com>
2021-09-19 18:26:01 +0800

    x

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func main() {
 	web.BConfig.WebConfig.Session.SessionName = models.AppName
 	go func() {
 		time.Sleep(time.Second * 4)
-		(&models.JdCookie{}).Push("小弟弟起来啦~")
+		(&models.JdCookie{}).Push("小弟弟起来了哦~")
 	}()
 	if models.Config.QQID != 0 || models.Config.QQGroupID != 0 {
 		go qbot.Main()
--- models/asset.go
+++ models/asset.go
@@ -72,7 +72,7 @@ func (ck *JdCookie) Query1() string {
 	msg := runTask(&Task{Path: name, Envs: envs}, &Sender{})
 	//log.Info(msg)
 	if !strings.Contains(msg, "cookies") {
-		msg = regexp.MustCompile(`^(.+\s+){2}|\s*.+\s*$|.*东东工厂.*\s*`).ReplaceAllString(msg, "")
+		msg = regexp.MustCompile(`^(.+\s+){3}|\s*.+\s*$|.*东东工厂.*\s*`).ReplaceAllString(msg, "")
 		msg = fmt.Sprintf("账号昵称：%s\n绑定QQ: %v\n用户等级：%v\n等级名称：%v\n更新时间: %s\n%s", ck.Nickname, ck.QQ, ck.UserLevel, ck.LevelName, ck.CreateAt, msg)
 	} else if CookieOK(ck) {
 		msg = fmt.Sprintf("查询失败\n账号: %s\n备注: %s\n%s", ck.PtPin, ck.Note, msg)
