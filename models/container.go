f95a24e9d8284c68957301edc4dd5b32f190e463
langhun <langhun@gmail.com>
2021-09-19 11:50:09 +0800

    x

--- models/container.go
+++ models/container.go
@@ -114,7 +114,7 @@ func initContainer() {
 
 }
 
-func (c *Container) Write(cks []JdCookie) error {
+func (c *Container) write(cks []JdCookie) error {
 	switch c.Type {
 	case "ql":
 		if c.Version == "2.8" || c.Version == "2.9" {
